a95c4881edb9d2cf9c00c97183dc79040c537c12
saidydnnsoft <said.nader@ydn.com.co>
2024-04-20 10:18:22 -0500

    chore: env name in app.py

--- src/app.py
+++ src/app.py
@@ -5,6 +5,6 @@ from flask_restful import Api
 from src import create_app
 from src.routes.routes import add_resources
 
-app = create_app("test")
+app = create_app("dev")
 api = Api(app)
 add_resources(api)
